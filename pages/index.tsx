4e9fc5b1e8f782f0227839ca6a291dc736db69ce
rebown <kiril270405@gmail.com>
2023-05-29 09:31:09 +0300

    adding global styles and background

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
+import {
+  BackgroundImage1,
+  BackgroundImage2,
+  GradientBackgroundCon,
+} from "@/components/QuoteGenerator/QuoteGeneratorElements";
+import Cloud1 from "@/assets/cloudy-weather.png";
+import Cloud2 from "@/assets/cloud-and-thunder.png";
+
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
+      <GradientBackgroundCon>
+        <BackgroundImage1 src={Cloud1} height="300" alt="img 1" />
+        <BackgroundImage2 src={Cloud2} height="300" alt="img 2" />
+      </GradientBackgroundCon>
     </>
   );
 }
