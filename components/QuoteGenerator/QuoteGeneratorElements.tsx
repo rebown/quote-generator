d81cdf8f2fc0dc165e2713168a3d5b09b0c85e31
rebown <kiril270405@gmail.com>
2023-05-29 09:40:03 +0300

    added footer and styled elements

---
+++ components/QuoteGenerator/QuoteGeneratorElements.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import styled from "styled-components";
+
+export const GradientBackgroundCon = styled.div`
+  background: linear-gradient(to right, #000046, #1cb5e0);
+  background-size: 400% 400%;
+  animation: gradient 6s ease infinite;
+  height: 100vh;
+  width: 100vw;
+  @keyframes gradient {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
+`;
+
+export const BackgroundImage1 = styled(Image)`
+  position: relative;
+  z-index: 1;
+  margin-left: -10px;
+  margin-top: -10px;
+`;
+
+export const BackgroundImage2 = styled(Image)`
+  position: fixed;
+  z-index: 1;
+  right: -120px;
+  bottom: -10px;
+`;
+
+export const FooterCon = styled.div`
+  width: 100vw;
+  height: 50px;
+  text-align: center;
+  font-family: "Source Code Pro", monospace;
+  font-size: 15px;
+  position: absolute;
+  bottom: 0;
+  color: white;
+  z-index: 999999;
+`;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,25 @@
 import {
   BackgroundImage1,
   BackgroundImage2,
+  FooterCon,
   GradientBackgroundCon,
 } from "@/components/QuoteGenerator/QuoteGeneratorElements";
 import Cloud1 from "@/assets/cloudy-weather.png";
 import Cloud2 from "@/assets/cloud-and-thunder.png";
+import { useState } from "react";
 
 export default function Home() {
+  const [numOfQuotes, setNumOfQuotes] = useState<Number | null>(0);
+
   return (
     <>
       <GradientBackgroundCon>
         <BackgroundImage1 src={Cloud1} height="300" alt="img 1" />
         <BackgroundImage2 src={Cloud2} height="300" alt="img 2" />
+
+        <FooterCon>
+          <>Quotes Generated: {numOfQuotes}</>
+        </FooterCon>
       </GradientBackgroundCon>
     </>
   );
